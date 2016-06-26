df24339b819caad0f83f33b8e95e349aceff3261
Alexa Herrera <alexa.herrera@digitaloperative.com>
2016-06-26 12:15:19 -0700

    Now you have how many problems?

---
+++ imports/imports.go
@@ -0,0 +1,10 @@
+package imports
+
+import (
+	"fmt"
+	"math"
+)
+
+func MathIsNeat() {
+	fmt.Println("Now you have %g problems", math.Sqrt(7))
+}
--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"time"
 	"github.com/anherrera/tour/packages"
+	"github.com/anherrera/tour/imports"
 )
 
 func main() {
 	fmt.Println("Welcome!")
 	fmt.Println("The time is", time.Now())
 	packages.RandomNumberPrint()
+	imports.MathIsNeat()
 }
