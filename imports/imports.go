daf955f6a4565e5a9e3a01d9c28c2702dadcdf89
Alexa Herrera <alexa.herrera@digitaloperative.com>
2016-06-26 17:02:15 -0700

    Structs are cool too.

--- imports/imports.go
+++ imports/imports.go
@@ -3,8 +3,39 @@ package imports
 import (
 	"fmt"
 	"math"
+	"github.com/darkhelmet/env"
 )
 
 func MathIsNeat() {
-	fmt.Println("Now you have %g problems", math.Sqrt(7))
+	fmt.Println("Pi is like, around", math.Pi)
+	line := fmt.Sprintf("Now you have %.5f problems", math.Sqrt(7))
+	fmt.Println(line)
+	fmt.Println("3 + 3 is", add(3, 3))
+	fmt.Println("Path is...", env.String("PATH"))
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+type Wife struct {
+	HowHerDayWas string
+	IsSheCute bool
+	IsSheMad bool
+}
+
+func (w *Wife) CookDinner() bool {
+	willSheCook := ! w.IsSheMad
+	return willSheCook
+}
+
+type Husband struct {
+	HowHisDayWas string
+	IsHeHandsome bool
+	IsHeSad bool
+}
+
+func (h *Husband) MowTheLawn() bool {
+	willHeDoIt := ! h.IsHeSad
+	return willHeDoIt
 }
--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"github.com/anherrera/tour/packages"
 	"github.com/anherrera/tour/imports"
+	"github.com/anherrera/tour/packages"
+	"time"
 )
 
 func main() {
@@ -12,4 +12,20 @@ func main() {
 	fmt.Println("The time is", time.Now())
 	packages.RandomNumberPrint()
 	imports.MathIsNeat()
+
+	var wife imports.Wife
+	wife.HowHerDayWas = "Alright, I suppose. Busy."
+	wife.IsSheCute = true
+	wife.IsSheMad = false
+
+	wife2 := &imports.Wife{"Super bad", true, true}
+	fmt.Println("The wife is mad?", wife2.IsSheMad)
+	fmt.Println("Will she cook then?", wife2.CookDinner())
+
+	var hubby imports.Husband
+	hubby.IsHeSad = true
+	fmt.Println("The husband is sad?", hubby.IsHeSad)
+	fmt.Println("Will he mow the lawn?", hubby.MowTheLawn())
+
+	fmt.Println("DONE-ZO!");
 }
